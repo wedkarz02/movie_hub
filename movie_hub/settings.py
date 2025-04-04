22174cdaf4e06aede9f632e0118bc0cfb189e002
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-04-04 22:23:06 +0200

    feat: users app, user register form

--- movie_hub/settings.py
+++ movie_hub/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hub.apps.HubConfig',
+    'users.apps.UsersConfig',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(req):
+    if req.method == "POST":
+        form = UserRegisterForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(req, f"Account created for {username}")
+            return redirect("hub-home")
+    else:
+        form = UserRegisterForm()
+
+    return render(req, "users/register.html", {"form": form, "title": "Register"})
